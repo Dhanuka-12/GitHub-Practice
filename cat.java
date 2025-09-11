6a84a0f396e45907c715c0be85a28951dc25a158
Dhanuka-12 <danukupe@gmail.com>
2025-09-11 23:37:52 +0530

    Created cat.java file and example.java. then created gitignore file to ignore all class files we do not need to track.

---
+++ cat.java
@@ -0,0 +1,10 @@
+class Cat{
+	public void speak(){
+		System.out.println("Meow...");
+	}
+	
+	public static void main(String[] args){
+		Cat obj = new Cat();
+		obj.speak();
+	}
+}
---
+++ example.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Student{
+	public void name(String name){
+		System.out.println("Your name is "+name);
+	}
+}
+
+class Main{
+	public static void main(String[] args){
+		Student std1 = new Student();
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Enter your name here : ");
+		String name = input.nextLine();
+		
+		System.out.println("");
+		
+		std1.name(name);
+			
+	}
+}
