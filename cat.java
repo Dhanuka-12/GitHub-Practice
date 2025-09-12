c3c81267c7caaf545d83ca86aad0ddec71147fde
Dhanuka-12 <danukupe@gmail.com>
2025-09-12 11:50:50 +0530

    changed that two files as one

--- cat.java
+++ cat.java
@@ -2,9 +2,4 @@ class Cat{
 	public void speak(){
 		System.out.println("Meow...");
 	}
-	
-	public static void main(String[] args){
-		Cat obj = new Cat();
-		obj.speak();
-	}
 }
--- example.java
+++ example.java
@@ -1,27 +1,7 @@
-import java.util.Scanner;
-
-class Student{
-	public void name(String name){
-		System.out.println("Your name is "+name);
-	}
-	
-	public void id(){
-		System.out.println("Your Id is : ");
-	}
-}
-
-class Main{
+class Example{
 	public static void main(String[] args){
-		Student std1 = new Student();
-		Scanner input = new Scanner(System.in);
-		
-		
-		System.out.print("Enter your name here : ");
-		String name = input.nextLine();
-		
-		System.out.println("");
-		
-		std1.name(name);
-			
+		System.out.println("Hello world");
+		Cat cat = new Cat();
+		cat.speak();
 	}
 }
